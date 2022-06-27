8336e56590ee0355d14f4f017f9e6c0dfaf939e5
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-27 16:44:24 +0300

    Add board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Board } from './components/Board';
 
 function App() {
   return (
     <div className="App">
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helpers";
+import { Square } from "../Square/Square";
+import "./Board.scss";
+
+export const Board: React.FC = () => {
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(" "));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (isXNext ? 'X' : 'O');
+  }
+
+  const handleClick = (i: number) => {
+    const newSquares = squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i] !== " ") {
+      return;
+    }
+
+    newSquares[i] = isXNext ? 'x' : 'o';
+
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+  }
+
+  return (
+    <section className="board">
+      <h1 className="board__title">{status}</h1>
+      <div className="board__grid">
+        {squares.map((square, index) => (
+          <Square
+            square={square}
+            handleClick={() => handleClick(index)}
+            key={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
